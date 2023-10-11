00c36fbbf26fec6712f46d75f261289fbad5d3a4
Siddeshwar <siddeshwar.work@gmail.com>
2023-10-11 18:57:25 +0530

    minor fix

--- frontend/src/components/booking/bookingCheckedin.tsx
+++ frontend/src/components/booking/bookingCheckedin.tsx
@@ -59,7 +59,7 @@ const BookingCheckedin: React.FC = () => {
                     <path d='M70 40C70 46.2683 68.0367 52.379 64.3853 57.4743C60.7343 62.5697 55.5787 66.393 49.6433 68.408C43.7077 70.4227 37.29 70.5277 31.2915 68.7083C25.2931 66.8887 20.0153 63.2357 16.1994 58.263C12.3835 53.29 10.2212 47.2467 10.0161 40.9817C9.81097 34.7167 11.5734 28.5448 15.0559 23.3329C18.5384 18.121 23.566 14.1308 29.4325 11.9228C35.299 9.71481 41.71 9.39987 47.7647 11.0222' stroke='#33363F' stroke-width='5' stroke-linecap='round' />
                 </svg>
             )}
-            caption='Your booking is completed'
+            caption='Your booking is checked-in'
         >
             <div style={{ textAlign: 'center', marginBottom: 16 }}>
                 <span className='dimmed'>Your vehicle is parked on</span>
--- src/main/java/com/sidapps/carparking/security/WebSecurityConfig.java
+++ src/main/java/com/sidapps/carparking/security/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
 		configuration.addAllowedMethod("*");
 		configuration.setAllowCredentials(true);
 		configuration.setAllowedHeaders(Arrays.asList("*"));
